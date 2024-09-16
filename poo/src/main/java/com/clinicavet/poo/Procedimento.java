84de3d4b47f5386ae5059321471ea43b69240140
Marina Mayumi Bastos <147656517+May4357@users.noreply.github.com>
2024-09-16 19:33:42 -0300

    Delete poo directory

--- poo/src/main/java/com/clinicavet/poo/Procedimento.java
+++
@@ -1,9 +0,0 @@
-package com.clinicavet.poo;
-// verificar se vai manter essa classe.
-public class Procedimento extends Medicovet {
-
-    private String tipoProcedimento; // verificar se haverá : (Consulta , vacina , procedimento cirurgico(castração) )
-    private String dataProcedimento;
-
-
-}
